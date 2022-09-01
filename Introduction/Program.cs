569f0a033632b8bd1bdfc747b26d813054d96b8c
Cactooz <54435874+Cactooz@users.noreply.github.com>
2022-09-01 10:46:36 +0200

    Add AccuracySum method

--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -14,7 +14,8 @@ class Introduction {
     public static void Main(String[] args) {
         Console.WriteLine("Started");
 
-        Accuracy();
+        //Accuracy();
+        AccuracySum();
 
         Console.WriteLine("Done");
     }
@@ -26,6 +27,21 @@ class Introduction {
     private static void Accuracy() {
         for(int i = 0; i < 10; i++) {
             long n0 = Stopwatch.GetTimestamp();
+            long n1 = Stopwatch.GetTimestamp();
+            Console.WriteLine($"Resolution {(n1 - n0) * nanosecondsPerTick} ns");
+        }
+    }
+
+    /// <summary>
+    /// Prints the time it takes for an addition operation from an array, 10 times.
+    /// </summary>
+    private static void AccuracySum() {
+        int[] given = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        int sum = 0;
+
+        for(int i = 0; i < 10; i++) {
+            long n0 = Stopwatch.GetTimestamp();
+            sum += given[i];
             long n1 = Stopwatch.GetTimestamp();
             Console.WriteLine($"Resolution {(n1 - n0) * nanosecondsPerTick} ns");
         }
