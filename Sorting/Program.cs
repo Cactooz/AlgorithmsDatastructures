e5fa770c2d1083dabd4ae6c68c69d280021230fa
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-09-10 12:19:34 +0200

    Add Utilities for swapping numbers and generating random array

---
+++ Sorting/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+namespace Sorting {
+    internal class Program {
+        static void Main(string[] args) {
+            long bubbleTime = 0;
+            long selectionTime = 0;
+            long insertionTime = 0;
+            int runAmount = 1000;
+            long nanosecondsPerTick = 1000000000 / Stopwatch.Frequency;
+
+            for(int i = 0; i < runAmount; i++) {
+                int[] bubble = Utilities.RandomArray(10);
+                int[] selection = Utilities.RandomArray(10);
+                int[] insertion = Utilities.RandomArray(10);
+
+                long t0 = Stopwatch.GetTimestamp();
+                BubbleSort.Sort(bubble);
+                long t1 = Stopwatch.GetTimestamp();
+
+                bubbleTime += (t1 - t0) * nanosecondsPerTick;
+
+
+                long t2 = Stopwatch.GetTimestamp();
+                SelectionSort.Sort(selection);
+                long t3 = Stopwatch.GetTimestamp();
+
+                selectionTime += (t3 - t2) * nanosecondsPerTick;
+
+                long t4 = Stopwatch.GetTimestamp();
+                InsertionSort.Sort(insertion);
+                long t5 = Stopwatch.GetTimestamp();
+
+                insertionTime += (t5 - t4) * nanosecondsPerTick;
+            }
+
+            Console.WriteLine($"Bubble: {bubbleTime / runAmount}ns");
+            Console.WriteLine($"Selection: {selectionTime / runAmount}ns");
+            Console.WriteLine($"Insertion: {insertionTime / runAmount}ns");
+        }
+    }
+}
---
+++ Sorting/Utilities.cs
@@ -0,0 +1,25 @@
+﻿namespace Sorting {
+    class Utilities {
+        /// <summary>
+        /// Swaps the two inputed numbers with eachother.
+        /// </summary>
+        public static void Swap(ref int a, ref int b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        /// <summary>
+        /// Creates a array with the inputted <c>size</c> and fills it with random numbers.
+        /// </summary>
+        /// <param name="size">The size of the array.</param>
+        /// <returns>Array with random numbers.</returns>
+        public static int[] RandomArray(int size) {
+            Random random = new Random();
+            int[] array = new int[size];
+            for(int i = 0; i < array.Length; i++)
+                array[i] = random.Next(array.Length*5);
+            return array;
+        }
+    }
+}
