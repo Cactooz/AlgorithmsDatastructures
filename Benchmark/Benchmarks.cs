1534c05e7d48207cf50dfe088d3b79e9e18ac32c
Cactooz <54435874+Cactooz@users.noreply.github.com>
2022-09-12 20:51:30 +0200

    Add Minimum method for benchmarking minimum time

--- Benchmark/Benchmarks.cs
+++ Benchmark/Benchmarks.cs
@@ -42,6 +42,42 @@ namespace Benchmark {
         }
 
         /// <summary>
+        /// Benchmark the minimum time it takes to execute a method with arrays.
+        /// </summary>
+        /// <param name="BenchmarkMethod">The method that should be benchmarked.</param>
+        /// <param name="type"> If the output should be for a <c>table</c>, <c>graph</c> or just print it.</param>
+        /// <param name="runAmount">The amount of times each test should be run.</param>
+        /// <param name="minSize">The minimum size of the array that should be tested.</param>
+        /// <param name="maxSize">The maximum size of the array that should be tested.</param>
+        /// <param name="increaseSize">What the last size should be multiplied by for the next array size.</param>
+        /// <returns>Output formatted for LATEX table or graph, or just a simple print.</returns>
+        public static void Minimum(Func<int[], int[]> BenchmarkMethod, string type, int runAmount, int minSize, int maxSize, int increaseSize) {
+            long minTime = 0;
+            string output = "";
+
+            for(int i = minSize; i < maxSize; i *= increaseSize) {
+                for(int j = 0; j < runAmount; j++) {
+                    int[] array = RandomArray(i);
+
+                    long t0 = Stopwatch.GetTimestamp();
+                    BenchmarkMethod(array);
+                    long t1 = Stopwatch.GetTimestamp();
+
+                    long time = (t1 - t0) * nanosecondsPerTick;
+                    if(time < minTime)
+                        minTime = time;
+                }
+                if(type.ToLower() == "table")
+                    output += $"{i} & {minTime / runAmount}\\\\\n";
+                else if(type.ToLower() == "graph")
+                    output += $"({i},{minTime / runAmount})";
+                else
+                    Console.WriteLine($"{i}: {minTime / runAmount}ns");
+            }
+            Console.WriteLine(output);
+        }
+
+        /// <summary>
         /// Creates a array with the inputted <c>size</c> and fills it with random numbers.
         /// </summary>
         /// <param name="size">The size of the array.</param>
