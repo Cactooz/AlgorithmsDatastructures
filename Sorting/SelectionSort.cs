afab509d5c98b623270fc3b4372f3ffef7c09f31
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-09-15 08:55:58 +0200

    Fix SelectionSort

--- Sorting/SelectionSort.cs
+++ Sorting/SelectionSort.cs
@@ -6,14 +6,19 @@
         /// <param name="array"><c>Array</c> to be sorted.</param>
         /// <returns>The sorted array.</returns>
         public static int[] Sort(int[] array) {
+            int length = array.Length;
+
             //Loop through the full array
-            for(int i = 0; i < array.Length - 1; i++) {
-                //Find the next smallest number and put it as early as possible
-                for(int j = i + 1; j < array.Length; j++) {
-                    //Swap the numbers if the number is smaller than the current one
+            for(int i = 0; i < length - 1; i++) {
+                int minIndex = i;
+                //Find the next smallest number and set the swap index
+                for(int j = i + 1; j < length; j++) {
+                    if(array[j] < array[minIndex])
+                        minIndex = j;
+                }
+                //Swap the numbers if the number is smaller than the current one
                     if(array[j] < array[i])
                         Utilities.Swap(ref array[i], ref array[j]);
-                }
             }
             return array;
         }
