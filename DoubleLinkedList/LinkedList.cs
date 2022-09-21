041d75eb34dd699b2a15ebe17c33a60837f5ca5c
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-09-21 09:11:43 +0200

    Add LinkedList using ListElement

--- DoubleLinkedList/LinkedList.cs
+++ DoubleLinkedList/LinkedList.cs
@@ -1,55 +1,93 @@
 ﻿namespace DoubleLinkedList {
     internal class LinkedList {
         /// <summary>
-        /// The value of the current <seealso cref="LinkedList"/> element.
+        /// The linked list contaning all the <see cref="ListElement"/>.
         /// </summary>
-        private int value;
-        /// <summary>
-        /// Reference to the next <seealso cref="LinkedList"/> element.
-        /// </summary>
-        private LinkedList next;
-        /// <summary>
-        /// Reference to the previous <seealso cref="LinkedList"/> element.
-        /// </summary>
-        private LinkedList previous;
+        private ListElement list;
 
         /// <summary>
-        /// Constructor for <seealso cref="LinkedList"/>.
+        /// Constructor for <see cref="LinkedList"/>.
+        /// Creates a new linked list with the inputted size, 
+        /// with each <see cref="ListElement"/> value getting incremented by one each time.
         /// </summary>
-        /// <param name="value">The value of the current item.</param>
-        /// <param name="next">A reference to the next element.</param>
-        /// <param name="previous">A reference to the previous element.</param>
-        public LinkedList(int value, LinkedList next, LinkedList previous) {
-            this.value = value;
-            this.next = next;
-            this.previous = previous;
+        /// <param name="size">The amount of elements that should be in the list.</param>
+        public LinkedList(int size) {
+            list = new ListElement(0, null, null);
+
+            //Pointer to the list to not change the original list
+            ListElement pointer = list;
+
+            //Loop through and add next references to create the list with the size
+            for(int i = 0; i < size - 1; i++) {
+                pointer.SetNext(new ListElement(i + 1, null, pointer));
+                pointer = pointer.GetNext();
+            }
         }
 
         /// <summary>
-        /// Get the value of the current <seealso cref="LinkedList"/> element.
+        /// Removes a <see cref="ListElement"/> from the specified position.
         /// </summary>
-        /// <returns>A int value of the current element.</returns>
-        public int GetValue() => value;
+        /// <param name="pos">The position of the element in the list.</param>
+        /// <returns>The <see cref="ListElement.value">value</see> of the element that got removed.</returns>
+        public int Remove(int pos) {
+            //Set the pointer to the beginning of the linked list.
+            ListElement pointer = list;
+
+            //Go to the correct position in the linked list.
+            for(int i = 0; i < pos; i++)
+                pointer = pointer.GetNext();
+
+            //Remove the references to the surrounding elements.
+            pointer.GetPrevious().SetNext(pointer.GetNext());
+            pointer.GetNext().SetPrevious(pointer.GetPrevious());
+
+            //Return the value of the removed element.
+            return pointer.GetValue();
+        }
+
         /// <summary>
-        /// Get the reference to the next <seealso cref="LinkedList"/> element.
+        /// Add a new <see cref="ListElement"/> to the beginning of the linked list,
+        /// with the inputted value.
         /// </summary>
-        /// <returns>Reference to the next element.</returns>
-        public LinkedList GetNext() => next;
+        /// <param name="value">The value that the element should have.</param>
+        public void Add(int value) {
+            //Add a new ListElement with the inputted value and next reference to current start element.
+            ListElement newList = new ListElement(value, list, null);
+
+            //Set the previous ListElement of the current start element.
+            list.SetPrevious(newList);
+
+            //Move the start of the linked list back to the newly added element.
+            list = list.GetPrevious();
+        }
+
         /// <summary>
-        /// Set the reference to the next <seealso cref="LinkedList"/> element.
+        /// Removes element from the <see cref="LinkedList"/> and places it at the start.
         /// </summary>
-        /// <param name="value">The reference to the next element.</param>
-        public void SetNext(LinkedList reference) => next = reference;
+        /// <param name="position">The position of the element that should be moved.</param>
+        public void RemoveAdd(int position) {
+            PrintList();
+            int removedValue = Remove(position);
+            PrintList();
+            Add(removedValue);
+            PrintList();
+        }
+
         /// <summary>
-        /// Get the reference to the previous <seealso cref="LinkedList"/> element.
+        /// Print the contents of the <see cref="LinkedList"/>.
         /// </summary>
-        /// <returns>Reference to the previous element.</returns>
-        public LinkedList GetPrevious() => previous;
-        /// <summary>
-        /// Set the reference to the previous <seealso cref="LinkedList"/> element.
-        /// </summary>
-        /// <param name="value">The reference to the previous element.</param>
-        public void SetPrevious(LinkedList reference) => previous = reference;
+        public void PrintList() {
+            //Set the pointer to the beginning of the linkedList.
+            ListElement pointer = list;
+
+            //Print all values of the elements in the linkedList.
+            Console.Write($"{{ {pointer.GetValue()}");
+            while(pointer.GetNext() != null) {
+                pointer = pointer.GetNext();
+                Console.Write($", {pointer.GetValue()}");
+            }
+            Console.WriteLine(" }");
+        }
 
     }
 }
---
+++ DoubleLinkedList/ListElement.cs
@@ -0,0 +1,54 @@
+﻿namespace DoubleLinkedList {
+    internal class ListElement {
+        /// <summary>
+        /// The value of the current <see cref="ListElement"/>.
+        /// </summary>
+        private int value;
+        /// <summary>
+        /// Reference to the next <see cref="ListElement"/>.
+        /// </summary>
+        private ListElement next;
+        /// <summary>
+        /// Reference to the previous <see cref="ListElement"/>.
+        /// </summary>
+        private ListElement previous;
+
+        /// <summary>
+        /// Constructor for <see cref="ListElement"/>.
+        /// </summary>
+        /// <param name="value">The value of the current item.</param>
+        /// <param name="next">A reference to the next element.</param>
+        /// <param name="previous">A reference to the previous element.</param>
+        public ListElement(int value, ListElement next, ListElement previous) {
+            this.value = value;
+            this.next = next;
+            this.previous = previous;
+        }
+
+        /// <summary>
+        /// Get the value of the current <see cref="ListElement"/>.
+        /// </summary>
+        /// <returns>A int value of the current element.</returns>
+        public int GetValue() => value;
+        /// <summary>
+        /// Get the reference to the next <see cref="ListElement"/>.
+        /// </summary>
+        /// <returns>Reference to the next element.</returns>
+        public ListElement GetNext() => next;
+        /// <summary>
+        /// Set the reference to the next <see cref="ListElement"/>.
+        /// </summary>
+        /// <param name="value">The reference to the next element.</param>
+        public void SetNext(ListElement reference) => next = reference;
+        /// <summary>
+        /// Get the reference to the previous <see cref="ListElement"/> element.
+        /// </summary>
+        /// <returns>Reference to the previous element.</returns>
+        public ListElement GetPrevious() => previous;
+        /// <summary>
+        /// Set the reference to the previous <see cref="ListElement"/> element.
+        /// </summary>
+        /// <param name="value">The reference to the previous element.</param>
+        public void SetPrevious(ListElement reference) => previous = reference;
+    }
+}
