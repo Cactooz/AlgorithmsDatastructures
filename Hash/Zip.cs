2b86d1ecc775f1ee59ffae84ed17b6062a8e3a2e
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-10-17 10:20:19 +0200

    Add Entry for Zip for keeping data

---
+++ Hash/Zip.cs
@@ -0,0 +1,21 @@
+﻿namespace Hash {
+	internal class Zip {
+		private class Entry {
+			private string zipCode;
+			private string zipLocation;
+			private int zipPopulation;
+
+			public Entry(string code, string location, int population) {
+				zipCode = code;
+				zipLocation = location;
+				zipPopulation = population;
+			}
+
+			public string ZipCode { get => zipCode; }
+			public string ZipLocation { get => zipLocation; }
+			public int ZipPopulation { get => zipPopulation; }
+
+		}
+
+	}
+}
