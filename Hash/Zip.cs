db1268672594d6a1c35203cfaef2e3ce1e58c732
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-10-17 12:44:47 +0200

    Add Zip constructor reading all file lines

--- Hash/Zip.cs
+++ Hash/Zip.cs
@@ -17,5 +17,24 @@
 
 		}
 
+		Entry[] data;
+		private int max;
+
+		public Zip(string file) {
+			int lines = File.ReadAllLines(file).Count();
+			data = new Entry[10000];
+
+			using(StreamReader reader = new StreamReader(file)) {
+				string line;
+				int i = 0;
+
+				while((line = reader.ReadLine()) != null) {
+					string[] row = line.Split(",");
+					data[i++] = new Entry(row[0], row[1], int.Parse(row[2]));
+				}
+				max = i - 1;
+			}
+		}
+
 	}
 }
