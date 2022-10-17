25d147f13c08f52eb6b64405c6828723f8fb5066
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-10-17 13:16:01 +0200

    Add benchmark for lookup

--- Hash/Program.cs
+++ Hash/Program.cs
@@ -1,7 +1,46 @@
-﻿namespace Hash {
+﻿using System.Diagnostics;
+
+namespace Hash {
 	internal class Program {
 		static void Main(string[] args) {
+			//Get the path to the .csv file containing all zipcodes
+			string file = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\zipcodes.csv"));
+			Zip zip = new Zip(file);
 
+			//Variable for converting Stopwatch.GetTimestamp output to nanoseconds.
+			long nanosecondsPerTick = 1000000000 / Stopwatch.Frequency;
+			//The amount of times to run the tests
+			int runAmount = 1000;
+			int averageAmount = 10;
+
+			long minTime = long.MaxValue;
+
+			//Run the benchmark runAmount times and take the mintime
+			for(int i = 0; i < runAmount; i++) {
+
+				long time = 0;
+
+				//Run the test averageAmount times ang get averages
+				for(int j = 0; j < averageAmount; j++) {
+					//Benchmark start
+					long t0 = Stopwatch.GetTimestamp();
+
+					zip.Lookup("994 99");
+
+					long t1 = Stopwatch.GetTimestamp();
+
+					//Save the total time
+					time += (t1 - t0) * nanosecondsPerTick;
+				}
+
+				//Get the average time
+				time /= averageAmount;
+
+				if(time < minTime)
+					minTime = time;
+			}
+
+			Console.WriteLine($"{minTime} ns");
 		}
 	}
 }
