bb723c0c233edaee819f2be5d546a4ecc497bc88
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-10-23 17:51:11 +0200

    Add Base constructor for Graph to add cities from file

---
+++ Graphs/Graph.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Graphs {
+	internal class Graph {
+
+        City[] cities;
+
+		public Graph(string filepath) {
+            //Read all connections from the file and add all cities and connections
+            using(StreamReader reader = new StreamReader(filepath)) {
+                //Get the amount of lines in the file
+                int lines = 0;
+                while(reader.ReadLine() != null)
+                    lines++;
+
+                cities = new City[lines + 1];
+
+                //Move the reader to the start
+                reader.BaseStream.Position = 0;
+
+                string line;
+                int i = 0;
+
+                //Read all lines
+                while((line = reader.ReadLine()) != null) {
+                    //Split the read line
+                    string[] row = line.Split(",");
+
+                    int length = int.Parse(Regex.Replace(row[2], @"\s+", ""));
+
+                    //Add the Entry to the data array 
+                    cities[i++] = new City(row[0]);
+                    //Add the Entry to the data array 
+                    cities[i++] = new City(row[1]);
+                }
+            }
+        }
+
+	}
+}
--- Graphs/Program.cs
+++ Graphs/Program.cs
@@ -1,7 +1,10 @@
 ﻿namespace Graphs {
 	internal class Program {
 		static void Main(string[] args) {
+            //Get the path to the connections file
+            string file = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\trains.csv"));
+            Graph graph = new Graph(file);
 
-		}
+        }
 	}
 }
