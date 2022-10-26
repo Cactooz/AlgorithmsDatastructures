b227306147c1b8d93b789f2b57f06bc9909944fe
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-10-26 18:27:06 +0200

    Move Naive ShortestPath to own class file

---
+++ Graphs/Naive.cs
@@ -0,0 +1,49 @@
+﻿namespace Graphs {
+	/// <summary>
+	/// A Naive way of finding the shortest path between two <see cref="City"/> objects.
+	/// </summary>
+	internal class Naive {
+		/// <summary>
+		/// Get the shortest path from the start <see cref="City"/> to the
+		/// end <see cref="City"/>, with the maximum length.
+		/// </summary>
+		/// <param name="from">The <see cref="City"/> to start in.</param>
+		/// <param name="to">The <see cref="City"/> to end at.</param>
+		/// <param name="max">The max length between the two <see cref="City"/>.</param>
+		/// <returns><see cref="Nullable"/> <see cref="int"/> of the shortest path.</returns>
+		public static int? ShortestPath(City from, City to, int max) {
+			if(max < 0)
+				return null;
+			if(from == to)
+				return 0;
+
+			//Save the shortest route
+			int? shortest = null;
+
+			//Loop through all connections
+			for(int i = 0; i < from.Connections.Length; i++) {
+				if(from.Connections[i] != null) {
+					City.Connection connection = from.Connections[i];
+
+					//Add the distance to the next city
+					int? addShort = connection.Length;
+
+					//Continue recursively down and find the shortest path
+					int? distance = ShortestPath(connection.EndCity, to, max - connection.Length);
+
+					//Add the returned path if its not null
+					if(distance != null)
+						addShort += distance;
+					else
+						continue;
+
+					//Override the shortest if the new one is shorter
+					if(addShort < shortest || shortest == null)
+						shortest = addShort;
+				}
+			}
+
+			return shortest;
+		}
+	}
+}
--- Graphs/Program.cs
+++ Graphs/Program.cs
@@ -3,68 +3,25 @@
 namespace Graphs {
 	internal class Program {
 		static void Main(string[] args) {
-            //Variable for converting Stopwatch.GetTimestamp output to nanoseconds.
-            long nanosecondsPerTick = 1000000000 / Stopwatch.Frequency;
+			//Variable for converting Stopwatch.GetTimestamp output to nanoseconds.
+			long nanosecondsPerTick = 1000000000 / Stopwatch.Frequency;
 
-            //Get the path to the connections file
-            string file = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\trains.csv"));
+			//Get the path to the connections file
+			string file = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\trains.csv"));
 			Graph map = new Graph(file);
 
 			string from = "Malmö";
 			string to = "Stockholm";
 			int max = 100;
 
-            long t0 = Stopwatch.GetTimestamp();
-            int? distance = ShortestPath(map.Lookup(from), map.Lookup(to), max);
-            long t1 = Stopwatch.GetTimestamp();
+			long t0 = Stopwatch.GetTimestamp();
+			int? distance = Naive.ShortestPath(map.Lookup(from), map.Lookup(to), max);
+			long t1 = Stopwatch.GetTimestamp();
 
 			if(distance.HasValue)
 				Console.WriteLine($"Shortest: {distance} ({(t1 - t0) * nanosecondsPerTick}ns)");
 			else
 				Console.WriteLine($"No path found with max {max} distance ({(t1 - t0) * nanosecondsPerTick}ns)");
-        }
-
-		/// <summary>
-		/// Get the shortest path from the start <see cref="City"/> to the
-		/// end <see cref="City"/>, with the maximum length.
-		/// </summary>
-		/// <param name="from">The <see cref="City"/> to start in.</param>
-		/// <param name="to">The <see cref="City"/> to end at.</param>
-		/// <param name="max">The max length between the two <see cref="City"/>.</param>
-		/// <returns><see cref="Nullable"/> <see cref="int"/> of the shortest path.</returns>
-		private static int? ShortestPath(City from, City to, int max) {
-			if(max < 0)
-				return null;
-			if(from == to)
-				return 0;
-
-			//Save the shortest route
-			int? shortest = null;
-
-			//Loop through all connections
-			for(int i = 0; i < from.Connections.Length; i++) {
-				if(from.Connections[i] != null) {
-					City.Connection connection = from.Connections[i];
-
-					//Add the distance to the next city
-					int? addShort = connection.Length;
-
-					//Continue recursively down and find the shortest path
-					int? distance = ShortestPath(connection.EndCity, to, max - connection.Length);
-
-					//Add the returned path if its not null
-					if(distance != null)
-						addShort += distance;
-					else
-						continue;
-
-					//Override the shortest if the new one is shorter
-					if(addShort < shortest || shortest == null)
-						shortest = addShort;
-				}
-			}
-
-			return shortest;
 		}
 	}
 }
