37d2d6fc05129450ba6a8f32a987c371fa911675
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-10-26 16:42:41 +0200

    Write ShortestPath search with time

--- Graphs/Program.cs
+++ Graphs/Program.cs
@@ -1,16 +1,28 @@
-﻿namespace Graphs {
+﻿using System.Diagnostics;
+
+namespace Graphs {
 	internal class Program {
 		static void Main(string[] args) {
-			//Get the path to the connections file
-			string file = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\trains.csv"));
+            //Variable for converting Stopwatch.GetTimestamp output to nanoseconds.
+            long nanosecondsPerTick = 1000000000 / Stopwatch.Frequency;
+
+            //Get the path to the connections file
+            string file = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\trains.csv"));
 			Graph map = new Graph(file);
 
 			string from = "Malmö";
 			string to = "Stockholm";
 			int max = 100;
 
-			Console.WriteLine(ShortestPath(map.Lookup(from), map.Lookup(to), max));
-		}
+            long t0 = Stopwatch.GetTimestamp();
+            int? distance = ShortestPath(map.Lookup(from), map.Lookup(to), max);
+            long t1 = Stopwatch.GetTimestamp();
+
+			if(distance.HasValue)
+				Console.WriteLine($"Shortest: {distance} ({(t1 - t0) * nanosecondsPerTick}ns)");
+			else
+				Console.WriteLine($"No path found with max {max} distance ({(t1 - t0) * nanosecondsPerTick}ns)");
+        }
 
 		/// <summary>
 		/// Get the shortest path from the start <see cref="City"/> to the
