67f851e032b7b8a3e349261f41133ff9a7eb3b2b
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-10-27 22:35:49 +0200

    Add Graph with extra id for City creation

---
+++ Dijkstra/Graph.cs
@@ -0,0 +1,127 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Dijkstra {
+	internal class Graph {
+		/// <summary>
+		/// Linked <see cref="Buckets"/> containing a <see cref="City"/>.
+		/// Linking to the next <see cref="Buckets"/>.
+		/// </summary>
+		private class Buckets {
+			private readonly City city;
+			private Buckets? next;
+
+			/// <summary>
+			/// Creates a new <see cref="City"/> and sets <see cref="next"/> to <c>null</c>.
+			/// </summary>
+			/// <param name="name">The name of the new <see cref="City"/>.</param>
+			/// <param name="id">The <see cref="City.id"/> value used for <see cref="Dijkstra.done"/> <see cref="Array"/> index.</param>
+			public Buckets(string name, int id) {
+				city = new City(name, id);
+				next = null;
+			}
+
+			/// <summary>
+			/// The current <see cref="City"/>.
+			/// </summary>
+			public City City { get => city; }
+			/// <summary>
+			/// Links to the next <see cref="Buckets"/> with another <see cref="City"/>.
+			/// </summary>
+			public Buckets? Next { get => next; set => next = value; }
+		}
+
+		/// <summary>
+		/// Linked <see cref="Buckets"/> containing all <see cref="City"/> objects.
+		/// </summary>
+		private Buckets[] cities;
+		private readonly int mod;
+		private int id;
+
+		/// <summary>
+		/// Read the whole <paramref name="filepath"/> and add all <see cref="City"/> objects
+		/// with all <see cref="City.Connection"/> between them.
+		/// </summary>
+		/// <param name="filepath">The full file path to the file with data.</param>
+		public Graph(string filepath) {
+			//Read all connections from the file and add all cities and connections
+			using(StreamReader reader = new(filepath)) {
+				mod = 541;
+				cities = new Buckets[mod];
+				//Start the id count at 0
+				id = 0;
+
+				string line;
+
+				//Read all lines
+				while((line = reader.ReadLine()) != null) {
+					//Split the read line
+					string[] row = line.Split(",");
+
+					//Create or find the two cities
+					City city1 = Lookup(row[0]);
+					City city2 = Lookup(row[1]);
+
+					//Convert the length of the connection to int
+					int length = int.Parse(Regex.Replace(row[2], @"\s+", ""));
+
+					//Add the connections between the two cities
+					city1.AddConnection(city2, length);
+					city2.AddConnection(city1, length);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Check if a <see cref="City"/> exist, otherwise create a new <see cref="City"/>
+		/// at the correct location in the <see cref="cities"/> <see cref="Array"/> of <see cref="Buckets"/>.
+		/// </summary>
+		/// <param name="name">The name of the new <see cref="City"/>.</param>
+		/// <returns>The searched for or added <see cref="City"/>.</returns>
+		public City Lookup(string name) {
+			int index = Hash(name);
+
+			//Create a city if there are no yet
+			if(cities[index] == null) {
+                //Add the new city and increase the id to the next city
+                cities[index] = new Buckets(name, id++);
+				return cities[index].City;
+			}
+
+			//If there already is a city at the position check through the linked Buckets
+			if(cities[index].City.Name != name) {
+				//Pointer to look through the linked buckets
+				Buckets pointer = cities[index];
+
+				//Go to the last linked Buckets or stop if name is found
+				while(!pointer.City.Name.Equals(name) && pointer.Next != null)
+					pointer = pointer.Next!;
+
+				//Set the new City to the next bucket if it does not exist
+				if(!pointer.City.Name.Equals(name)) {
+                    //Add the new city and increase the id to the next city
+                    pointer.Next = new Buckets(name, id++);
+					return pointer.Next.City;
+				}
+				else
+					return pointer.City;
+			}
+
+			//Return the city.
+			return cities[index].City;
+		}
+
+		/// <summary>
+		/// Hash the <paramref name="name"/> using <see cref="mod"/>.
+		/// </summary>
+		/// <param name="name">The name to hash as a <see cref="string"/>.</param>
+		/// <returns>The hashed <paramref name="name"/> as a <see cref="int"/>.</returns>
+		private int Hash(string name) {
+			int hash = 7;
+			for(int i = 0; i < name.Length; i++)
+				hash = (hash * 31 % mod) + name[i];
+
+			return hash % mod;
+		}
+
+	}
+}
